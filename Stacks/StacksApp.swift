e9a1763d5664b9ea649456be0063f9b6f492e83b
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2020-11-14 22:52:02 -0300

    Initial Commit

---
+++ Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by Jacob Aguilar on 14-11-20.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
